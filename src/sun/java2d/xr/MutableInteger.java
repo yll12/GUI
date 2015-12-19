f6cfad0a561c08454276f6c127d39f3aa866be1f
yll12 <yong.loh12@imperial.ac.uk>
2015-12-19 21:58:04 +0000

    Folder layout rearranged

---
+++ src/sun/java2d/xr/MutableInteger.java
@@ -0,0 +1,21 @@
+package sun.java2d.xr;
+
+public class MutableInteger {
+
+    int x;
+    
+    public MutableInteger(int x) {
+        this.x = x;
+   }
+
+    public int getValue() {
+        return x;
+    }
+
+    public void setValue(int x) {
+        this.x = x;
+    }
+
+    
+    
+}
